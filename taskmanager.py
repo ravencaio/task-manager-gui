628a7f851da41b341aca71ee730d5ebe09f93d7e
Caio Eduardo de Albuquerque Magalhães <69921724+ravencaio@users.noreply.github.com>
2025-05-25 16:24:57 -0300

    Update taskmanager.py
    Added comments and small changes

--- taskmanager.py
+++ taskmanager.py
@@ -1,26 +1,25 @@
-import task_module
+import task_module        #Huge credits for Elanosinho (https://github.com/Elanosinho) for creating the task_module library
 import ttkbootstrap as ttk
 from ttkbootstrap.constants import *
 from ttkbootstrap.style import Style
 
 app = ttk.Window('Task Manager')
-#app.geometry('842x600')
-style = Style('solar')
+#app.geometry('842x600')     # Manipulates app window size, I don't recommend its usage as I personally like my app to adapt its size
+style = Style('solar')       # Changes the app's theme
 
 # Loads tasks.json into variable
 task_list = task_module.load_tasks()
 
 
-def task_update(task_id, status):
+def task_update(task_id, status):             # Updates the task's status and commits it to the JSON file
     task_list[str(task_id)]['status'] = status
     task_module.update_tasks(task_list, task_id)
-    print(f'atualizou task {task_id} com status de {status}')
     stat[str(task_id)].config(text=f' {status}')
 
 def delete_task(task_id):
-    del task_list[str(task_id)]
+    del task_list[str(task_id)]                          # Deletes task from JSON file
     task_module.update_tasks(task_list, task_id, delete=True)
-    t_stash[str(task_id)].destroy()
+    t_stash[str(task_id)].destroy()                      # Deletes task from the app interface
     t2_stash[str(task_id)].destroy()
     t3_stash[str(task_id)].destroy()
     t4_stash[str(task_id)].destroy()
@@ -30,9 +29,9 @@ def delete_task(task_id):
     b3_stash[str(task_id)].destroy()
     b4_stash[str(task_id)].destroy()
 
-    del t_stash[str(task_id)]
+    del t_stash[str(task_id)]                           # Deletes task from the stash dicts
     del t2_stash[str(task_id)]
-    del t3_stash[str(task_id)]
+    del t3_stash[str(task_id)] 
     del t4_stash[str(task_id)]
     del stat[str(task_id)]
     del b1_stash[str(task_id)]
@@ -44,13 +43,12 @@ def delete_task(task_id):
 
 
 def create_task():
-    row = len(t_stash.keys()) + 1
+    row = len(t_stash.keys()) + 1                  # Defines the task's row by the quantity of keys in t_stash
     task_id = task_module.generate_id(task_list)
     new_task = task_module.create_task(task_id, aname.get())
     task_list[str(task_id)] = new_task
-    task_module.update_tasks(task_list, task_id)
-    print('criou task')
-    t = ttk.Label(app, text=f'Task {task_list[str(task_id)]['id']} - ', font=('arial', 10))
+    task_module.update_tasks(task_list, task_id) 
+    t = ttk.Label(app, text=f'Task {task_list[str(task_id)]['id']} - ', font=('arial', 10))            # Altered task creation method
     t2 = ttk.Label(app, text=task_list[str(task_id)]['description'], font=('arial', 10))
     t3 = ttk.Label(app, text=f'- created: {task_list[str(task_id)]['created']} last updated: {task_list[str(task_id)]['last updated']} ', font=('arial', 10))
     t4 = ttk.Label(app, text=f'status:', font=('arial', 10))
@@ -85,7 +83,7 @@ def create_task():
 
 
 def add_interface():
-    global aname
+    global aname                                          # Creates a new window that allows the user to create a new task
     aname = ttk.StringVar()
     create = ttk.Toplevel('Create a new task')
     lc1 = ttk.Label(create, text='Name: ', font=('arial', 10))
@@ -97,7 +95,7 @@ def add_interface():
     bc1.grid(row=0, column=2)
 
 
-stat = {}
+stat = {}                                     # Important stash dicts for saving label and button data
 t_stash = {}
 t2_stash = {}
 t3_stash = {}
@@ -107,10 +105,10 @@ b2_stash = {}
 b3_stash = {}
 b4_stash = {}
 
-
+# Variable used to determine a task's row in the app's interface
 row = 0
 for task in task_list.values():
-    t = ttk.Label(app, text=f'Task {task['id']} - ', font=('arial', 10))
+    t = ttk.Label(app, text=f'Task {task['id']} - ', font=('arial', 10))              # Creates a task
     t2 = ttk.Label(app, text=task['description'], font=('arial', 10))
     t3 = ttk.Label(app, text=f'- created: {task['created']} last updated: {task['last updated']} ', font=('arial', 10))
     t4 = ttk.Label(app, text=f'status:', font=('arial', 10))
@@ -120,8 +118,8 @@ for task in task_list.values():
     b3 = ttk.Button(app, text='TO DO', bootstyle='warning', command=lambda j=task['id']:task_update(j, 'to do'))
     b4 = ttk.Button(app, text='X', bootstyle='danger', command=lambda j=task['id']:delete_task(j))
         
-    t_stash[f'{task['id']}'] = t
-    t2_stash[f'{task['id']}'] = t2
+    t_stash[f'{task['id']}'] = t                               # Append's tasks elements to the stash dicts
+    t2_stash[f'{task['id']}'] = t2                           
     t3_stash[f'{task['id']}'] = t3
     t4_stash[f'{task['id']}'] = t4
     b1_stash[f'{task['id']}'] = b1
@@ -130,7 +128,7 @@ for task in task_list.values():
     b4_stash[f'{task['id']}'] = b4
     stat[f'{task['id']}'] = t5
 
-    t.grid(row=row, column=0, sticky= W)
+    t.grid(row=row, column=0, sticky= W)                   # Grids the labels and buttons defined previously
     t2.grid(row= row, column= 1, padx= 5)
     t3.grid(row=row, column= 2)
     t4.grid(row=row, column=3)
@@ -142,11 +140,10 @@ for task in task_list.values():
     row +=1
 
     
-b5 = ttk.Button(app, text='NEW', bootstyle='success', command=add_interface)
+b5 = ttk.Button(app, text='NEW', bootstyle='success', command=add_interface)         # Button that creates a new task
 b5.grid(row=row, column= 0)
     
 
 
-app.resizable(width=False, height=False)
+app.resizable(width=False, height=False)              # Restrains user from changing the window's size
 app.mainloop()
-
